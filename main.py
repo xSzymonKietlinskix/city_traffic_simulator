2b74fa1e7c711f78ca3b87ede09a65bf0874ebeb
xSzymonKietlinskix <szymon.kietlinski@gmail.com>
2024-04-18 20:19:40 +0200

    changed graphics

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import random
+import sys
 import time
 from a_star import a_star_
-
 import pygame
 import numpy as np
 
 
 
-def generate_map(L):
-    city = np.random.choice([0, 1], size=(L, L), p=[0.6, 0.4])
+def generate_map(L, p):
+    city = np.random.choice([0, 1], size=(L, L), p=[1.0-p, p])
     return city
 
 def show_city(city, start, end, current):
@@ -16,7 +16,7 @@ def show_city(city, start, end, current):
     for i in range(L):
         for j in range(L):
             if start[0] == i and start[1] == j:
-                screen.blit(car_img, (i * cell_size, j * cell_size))
+                screen.blit(start_img, (i * cell_size, j * cell_size))
             elif current[0] == i and current[1] == j:
                 screen.blit(car_img, (i * cell_size, j * cell_size))
             elif end[0] == i and end[1] == j:
@@ -28,22 +28,29 @@ def show_city(city, start, end, current):
 
     pygame.display.flip()
 
+def show_menu():
+    print("Menu")
+
 if __name__ == '__main__':
+    show_menu()
     L = int(input("Podaj rozmiar siatki: "))
+    p = float(input("Podaj prawdopodobieństwo wystapienia blokady drogowej. (np. 0.5): "))
 
     start = (random.randint(0, L - 1), random.randint(0, L - 1))
     end = (random.randint(0, L - 1), random.randint(0, L - 1))
 
-    city = generate_map(L)
+    city = generate_map(L, p)
 
     while city[start] == 1:
         start = (random.randint(0, L - 1), random.randint(0, L - 1))
+    while city[start] == city[end]:
+        end = (random.randint(0, L - 1), random.randint(0, L - 1))
     while city[end] == 1:
         end = (random.randint(0, L - 1), random.randint(0, L - 1))
 
     pygame.init()
     BLACK = (0, 0, 0)
-    cell_size = 50
+    cell_size = 1920 / (3*L)
     window_size = (L * cell_size, L * cell_size)
     screen = pygame.display.set_mode(window_size)
     pygame.display.set_caption("Symulator Przejazdu Pojazdu")
@@ -52,11 +59,14 @@ if __name__ == '__main__':
     blokada_img = pygame.image.load("blokada.png").convert_alpha()
     car_img = pygame.image.load("car.png").convert_alpha()
     meta_img = pygame.image.load("meta.png").convert_alpha()
+    start_img = pygame.image.load("start.png").convert_alpha()
 
     droga_img = pygame.transform.scale(droga_img, (cell_size, cell_size))
     blokada_img = pygame.transform.scale(blokada_img, (cell_size, cell_size))
     car_img = pygame.transform.scale(car_img, (cell_size, cell_size))
     meta_img = pygame.transform.scale(meta_img, (cell_size, cell_size))
+    start_img = pygame.transform.scale(start_img, (cell_size, cell_size))
+
     path = a_star_(city, start, end, L)
 
     if path:
@@ -65,7 +75,8 @@ if __name__ == '__main__':
         print("Nie znaleziono ścieżki.")
 
     show_city(city, start, end, start)
-    time.sleep(1)
+    # time.sleep(1)
+
 
     running = True
     while running:
@@ -75,7 +86,7 @@ if __name__ == '__main__':
 
         for p in path:
             show_city(city, start, end, p)
-            time.sleep(1)
+            time.sleep(1.5)
 
         pygame.display.update()
 
